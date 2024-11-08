ebbb54e549541d84215e87e0f104aa5a608de4a7
jobserver <joabe.santos@gmail.com>
2024-11-08 10:27:25 -0300

    feat: criar rota perfil

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['login']['GET'] = 'login';
 $route['home']['GET'] = 'home';
+$route['perfil']['GET'] = 'perfil';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
