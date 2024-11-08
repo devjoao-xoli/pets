2ea60d62f371cb8eaa8116dd79c1200690729905
jobserver <joabe.santos@gmail.com>
2024-11-08 10:23:44 -0300

    feat: Restringir rota com login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,6 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		if (!$this->session->userdata('usuario')) {
+			redirect(base_url(). 'login');
+		}
+	}
+
 	public function index()
 	{
 		$data['user_name'] = $this->session->userdata('usuario')->user;
